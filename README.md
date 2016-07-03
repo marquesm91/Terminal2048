999271ad1d0e84c18d3201ba747fd652ed783db1
marquesm91 <marquesm91@gmail.com>
2016-07-03 17:37:46 -0300

    first impressions with ncurses

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 Created 10 June 2014 (Version 1.0)
 
-Console Application of 2048 Game using NCurses
+Console Application of 2048 Game using **Ncurses**
 
-## For Version 1.1
+## Contributors
 
-+ Improve codes
-+ Smooth movements
-+ Improve algorithms
-+ Improve colors
-+ Improve position screen
+Matheus Ferreira Marques
+
+## Future Tasks for Version 1.1
+
++ Learn how to use ncurses to draw boxes to make the background game
++ Learn how to use colors on ncurses
++ Improve random algorithm
++ Learn how to do smooth movements with or without ncurses
--- t2048_Linux.cpp
+++ t2048_Linux.cpp
@@ -490,7 +490,8 @@ bool Terminal2048::start()
 
 }
 
-int main(void)
+// Main Program
+/*int main(void)
 {
 	bool keep_playing;
 
@@ -506,4 +507,129 @@ int main(void)
 	} while (keep_playing == true);
 
 	return 0;
+}*/
+
+// NCurses tests main
+typedef struct _win_border_struct {
+	chtype 	left, right, top, under, 
+	 	topleft, topright, underleft, underright;
+}WIN_BORDER;
+
+typedef struct _WIN_struct {
+
+	int startx, starty;
+	int height, width;
+	WIN_BORDER border;
+}WIN;
+
+void init_win_params(WIN *p_win);
+void print_win_params(WIN *p_win);
+void create_box(WIN *win, bool flag);
+
+int main(int argc, char *argv[])
+{	WIN win;
+	int ch;
+
+	initscr();								// Start curses mode
+	start_color();							// Start the color functionality
+	cbreak();								// Line buffering disabled
+
+	curs_set(0);							// hide cursor console
+	keypad(stdscr, TRUE);					// For Arrow Keys
+	noecho();								// disable echo() in getch()
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+
+	// Initialize the window parameters
+	init_win_params(&win);
+	print_win_params(&win);
+
+	attron(COLOR_PAIR(1));
+	printw("Press E to exit");
+	refresh();
+	attroff(COLOR_PAIR(1));
+	
+	create_box(&win, TRUE);
+	while((ch = getch()) != 'E')
+	{	switch(ch)
+		{	case KEY_LEFT:
+				create_box(&win, FALSE);
+				--win.startx;
+				create_box(&win, TRUE);
+				break;
+			case KEY_RIGHT:
+				create_box(&win, FALSE);
+				++win.startx;
+				create_box(&win, TRUE);
+				break;
+			case KEY_UP:
+				create_box(&win, FALSE);
+				--win.starty;
+				create_box(&win, TRUE);
+				break;
+			case KEY_DOWN:
+				create_box(&win, FALSE);
+				++win.starty;
+				create_box(&win, TRUE);
+				break;	
+		}
+	}
+	endwin();			/* End curses mode		  */
+	return 0;
+}
+void init_win_params(WIN *p_win)
+{
+	p_win->height = 2;
+	p_win->width  = 3;
+
+	// LINES and COLS are set with the size screen when initscr() is called.
+	//p_win->starty = (LINES - p_win->height)/2;	
+	//p_win->startx = (COLS - p_win->width)/2;
+	p_win->starty = 1;
+	p_win->startx = 1;
+
+	p_win->border.left       = '|';
+	p_win->border.right      = '|';
+	p_win->border.top        = '-';
+	p_win->border.under      = '-';
+	p_win->border.topleft    = '+';
+	p_win->border.topright   = '+';
+	p_win->border.underleft  = '+';
+	p_win->border.underright = '+';
+
+}
+void print_win_params(WIN *p_win)
+{
+#ifdef _DEBUG
+	mvprintw(25, 0, "%d %d %d %d", p_win->startx, p_win->starty, 
+				p_win->width, p_win->height);
+	refresh();
+#endif
+}
+void create_box(WIN *p_win, bool flag)
+{	int i, j;
+	int x, y, w, h;
+
+	x = p_win->startx;
+	y = p_win->starty;
+	w = p_win->width;
+	h = p_win->height;
+
+	if(flag == TRUE)
+	{	mvaddch(y, x, p_win->border.topleft);
+		mvaddch(y, x + w, p_win->border.topright);
+		mvaddch(y + h, x, p_win->border.underleft);
+		mvaddch(y + h, x + w, p_win->border.underright);
+		mvhline(y, x + 1, p_win->border.top, w - 1);
+		mvhline(y + h, x + 1, p_win->border.under, w - 1);
+		mvvline(y + 1, x, p_win->border.left, h - 1);
+		mvvline(y + 1, x + w, p_win->border.right, h - 1);
+
+	}
+	else
+		for(j = y; j <= y + h; ++j)
+			for(i = x; i <= x + w; ++i)
+				mvaddch(j, i, ' ');
+				
+	refresh();
+
 }
